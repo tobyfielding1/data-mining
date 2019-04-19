2a8051e7ec63922046867ba973895fe979df1b20
Riddhi <riddhidhall@gmail.com>
2019-04-19 17:28:56 +0100

    created functions for aggregation and started on adding hand built features

--- src/main.py
+++ src/main.py
@@ -49,20 +49,86 @@ def make_processed_data():
     utils.save_pickle(dir + '\\..\\pre_processed_data\\bureau_balance_processed', bureau_balance)
 
     credit_card_balance, orig = pre_process(dir + '\\..\\..\\data\\credit_card_balance.csv', ['SK_ID_PREV', 'SK_ID_CURR'])
-    utils.save_pickle(dir + '\\..\\pre_processed_data\\credit_card_balance_processed', credit_card_balance)
+    utils.save_pickle(dir + '\\..\\pre_processed_data\\credit_card_processed', credit_card_balance)
 
     installments_payments, orig = pre_process(dir + '\\..\\..\\data\\installments_payments.csv', ['SK_ID_PREV', 'SK_ID_CURR'])
-    utils.save_pickle(dir + '\\..\\pre_processed_data\\installments_payments_processed', installments_payments)
+    utils.save_pickle(dir + '\\..\\pre_processed_data\\installments_processed', installments_payments)
 
     POS_CASH_balance, orig = pre_process(dir + '\\..\\..\\data\\POS_CASH_balance.csv', ['SK_ID_PREV', 'SK_ID_CURR'])
-    utils.save_pickle(dir + '\\..\\pre_processed_data\\POS_CASH_balance_processed', POS_CASH_balance)
+    utils.save_pickle(dir + '\\..\\pre_processed_data\\cash_processed', POS_CASH_balance)
 
     previous_application, orig = pre_process(dir + '\\..\\..\\data\\previous_application.csv', ['SK_ID_PREV', 'SK_ID_CURR'])
-    utils.save_pickle(dir + '\\..\\pre_processed_data\\previous_application_processed', previous_application)
+    utils.save_pickle(dir + '\\..\\pre_processed_data\\previous_processed', previous_application)
+
+
+def make_manual_features():
+    app = utils.load_pickle(dir + '\\..\\pre_processed_data\\app_train_processed')
+    bureau = utils.load_pickle(dir + '\\..\\pre_processed_data\\bureau_processed')
+    previous = utils.load_pickle(dir + '\\..\\pre_processed_data\\previous_processed')
+    bureau_balance = utils.load_pickle(dir + '\\..\\pre_processed_data\\bureau_balance_processed')
+    cash = utils.load_pickle(dir + '\\..\\pre_processed_data\\cash_processed')
+    credit = utils.load_pickle(dir + '\\..\\pre_processed_data\\credit_card_processed')
+    installments = utils.load_pickle(dir + '\\..\\pre_processed_data\\installments_processed')
+
+    app['LOAN_RATE'] = app['AMT_ANNUITY'] / app['AMT_CREDIT']
+    app['CREDIT_INCOME_RATIO'] = app['AMT_CREDIT'] / app['AMT_INCOME_TOTAL']
+    app['EMPLOYED_BIRTH_RATIO'] = app['DAYS_EMPLOYED'] / app['DAYS_BIRTH']
+    app['EXT_SOURCE_SUM'] = app[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']].sum(axis=1)
+    app['EXT_SOURCE_MEAN'] = app[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']].mean(axis=1)
+    app['AMT_REQ_SUM'] = app[[x for x in app.columns if 'AMT_REQ_' in x]].sum(axis=1)
+    bureau['LOAN_RATE'] = bureau['AMT_ANNUITY'] / bureau['AMT_CREDIT_SUM']
+
+    bureau_info = me.agg_child(bureau, 'SK_ID_CURR', 'BUREAU')
+
+    bureau_balance['PAST_DUE'] = bureau_balance['STATUS'].isin(['1', '2', '3', '4', '5'])
+    bureau_balance['ON_TIME'] = bureau_balance['STATUS'] == '0'
+    bureau_balance_info = me.agg_grandchild(bureau_balance, bureau, 'SK_ID_BUREAU', 'SK_ID_CURR', 'BB')
+    del bureau_balance, bureau
+
+    app = app.set_index('SK_ID_CURR')
+    app = app.merge(bureau_info, on='SK_ID_CURR', how='left')
+    del bureau_info
+
+    app = app.merge(bureau_balance_info, on='SK_ID_CURR', how='left')
+    del bureau_balance_info
+
+    previous['LOAN_RATE'] = previous['AMT_ANNUITY'] / previous['AMT_CREDIT']
+    previous["AMT_DIFFERENCE"] = previous['AMT_CREDIT'] - previous['AMT_APPLICATION']
+    previous_info = me.agg_child(previous, 'SK_ID_CURR', 'PREVIOUS')
+    app = app.merge(previous_info, on='SK_ID_CURR', how='left')
+    del previous_info
+
+    installments['LATE'] = installments['DAYS_ENTRY_PAYMENT'] > installments['DAYS_INSTALMENT']
+    installments['LOW_PAYMENT'] = installments['AMT_PAYMENT'] < installments['AMT_INSTALMENT']
+    installments_info = me.agg_grandchild(installments, previous, 'SK_ID_PREV', 'SK_ID_CURR', 'IN')
+    del installments
+    app = app.merge(installments_info, on='SK_ID_CURR', how='left')
+    del installments_info
+
+    cash['LATE_PAYMENT'] = cash['SK_DPD'] > 0.0
+    cash['INSTALLMENTS_PAID'] = cash['CNT_INSTALMENT'] - cash['CNT_INSTALMENT_FUTURE']
+    cash_info = me.agg_grandchild(cash, previous, 'SK_ID_PREV', 'SK_ID_CURR', 'CASH')
+    del cash
+    app = app.merge(cash_info, on='SK_ID_CURR', how='left')
+    del cash_info
+
+    credit['OVER_LIMIT'] = credit['AMT_BALANCE'] > credit['AMT_CREDIT_LIMIT_ACTUAL']
+    credit['BALANCE_CLEARED'] = credit['AMT_BALANCE'] == 0.0
+    credit['LOW_PAYMENT'] = credit['AMT_PAYMENT_CURRENT'] < credit['AMT_INST_MIN_REGULARITY']
+    credit['LATE'] = credit['SK_DPD'] > 0.0
+    credit_info = me.agg_grandchild(credit, previous, 'SK_ID_PREV', 'SK_ID_CURR', 'CC')
+    del credit, previous
+    app = app.merge(credit_info, on='SK_ID_CURR', how='left')
+    del credit_info
+
+    print('After manual feature engineering, there are {} features.'.format(app.shape[1] - 2))
 
 
 make_processed_data()  # only needs to be called once
 
-
+# call function to create manual features
+# find the correlation for each of these new features
+# add the features that have a higher correlation to the df
+# save the df as a pickle
 
 
--- src/manual_engineering.py
+++ src/manual_engineering.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import numpy as np
+import gc
 
 # income_per_child
 # days_with_same_bank
@@ -15,7 +16,225 @@ import pandas as pd
 # loan completed
 
 
-def get_mean(df, columns):
-    return ""
+def agg_numeric(df, parent_var, df_name):
+    """
+    Groups and aggregates the numeric values in a child dataframe
+    by the parent variable.
+
+    Parameters
+    --------
+        df (dataframe):
+            the child dataframe to calculate the statistics on
+        parent_var (string):
+            the parent variable used for grouping and aggregating
+        df_name (string):
+            the variable used to rename the columns
+
+    Return
+    --------
+        agg (dataframe):
+            a dataframe with the statistics aggregated by the `parent_var` for
+            all numeric columns. Each observation of the parent variable will have
+            one row in the dataframe with the parent variable as the index.
+            The columns are also renamed using the `df_name`. Columns with all duplicate
+            values are removed.
+
+    """
+
+    # Remove id variables other than grouping variable
+    for col in df:
+        if col != parent_var and 'SK_ID' in col:
+            df = df.drop(columns=col)
+
+    # Only want the numeric variables
+    parent_ids = df[parent_var].copy()
+    numeric_df = df.select_dtypes('number').copy()
+    numeric_df[parent_var] = parent_ids
+
+    # Group by the specified variable and calculate the statistics
+    agg = numeric_df.groupby(parent_var).agg(['count', 'mean', 'max', 'min', 'sum'])
+
+    # Need to create new column names
+    columns = []
+
+    # Iterate through the variables names
+    for var in agg.columns.levels[0]:
+        if var != parent_var:
+            # Iterate through the stat names
+            for stat in agg.columns.levels[1]:
+                # Make a new column name for the variable and stat
+                columns.append('%s_%s_%s' % (df_name, var, stat))
+
+    agg.columns = columns
+
+    # Remove the columns with all redundant values
+    _, idx = np.unique(agg, axis=1, return_index=True)
+    agg = agg.iloc[:, idx]
+
+    return agg
+
+
+def agg_categorical(df, parent_var, df_name):
+    """
+    Aggregates the categorical features in a child dataframe
+    for each observation of the parent variable.
+
+    Parameters
+    --------
+    df : dataframe
+        The dataframe to calculate the value counts for.
+
+    parent_var : string
+        The variable by which to group and aggregate the dataframe. For each unique
+        value of this variable, the final dataframe will have one row
+
+    df_name : string
+        Variable added to the front of column names to keep track of columns
+
+
+    Return
+    --------
+    categorical : dataframe
+        A dataframe with aggregated statistics for each observation of the parent_var
+        The columns are also renamed and columns with duplicate values are removed.
+
+    """
+
+    # Select the categorical columns
+    categorical = pd.get_dummies(df.select_dtypes('category'))
+
+    # Make sure to put the identifying id on the column
+    categorical[parent_var] = df[parent_var]
+
+    # Groupby the group var and calculate the sum and mean
+    categorical = categorical.groupby(parent_var).agg(['sum', 'count', 'mean'])
+
+    column_names = []
+
+    # Iterate through the columns in level 0
+    for var in categorical.columns.levels[0]:
+        # Iterate through the stats in level 1
+        for stat in ['sum', 'count', 'mean']:
+            # Make a new column name
+            column_names.append('%s_%s_%s' % (df_name, var, stat))
+
+    categorical.columns = column_names
+
+    # Remove duplicate columns by values
+    _, idx = np.unique(categorical, axis=1, return_index=True)
+    categorical = categorical.iloc[:, idx]
+
+    return categorical
+
+
+# can be called by bureau and previous as they are direct children of app
+def agg_child(df, parent_var, df_name):
+    """Aggregate a child dataframe for each observation of the parent."""
+
+    # Numeric and then categorical
+    df_agg = agg_numeric(df, parent_var, df_name)
+    df_agg_cat = agg_categorical(df, parent_var, df_name)
+
+    # Merge on the parent variable
+    df_info = df_agg.merge(df_agg_cat, on=parent_var, how='outer')
+
+    # Remove any columns with duplicate values
+    _, idx = np.unique(df_info, axis=1, return_index=True)
+    df_info = df_info.iloc[:, idx]
+
+    # memory management
+    gc.enable()
+    del df_agg, df_agg_cat
+    gc.collect()
+
+    return df_info
+
+
+# can be called for the child classes of the child classes which is bureau_balance, cash, credit_card and installments
+def agg_grandchild(df, parent_df, parent_var, grandparent_var, df_name):
+    """
+    Aggregate a grandchild dataframe at the grandparent level.
+
+    Parameters
+    --------
+        df : dataframe
+            Data with each row representing one observation
+
+        parent_df : dataframe
+            Parent table of df that must have the parent_var and
+            the grandparent_var. Used only to get the grandparent_var into
+            the dataframe after aggregations
+
+        parent_var : string
+            Variable representing each unique observation in the parent.
+            For example, `SK_ID_BUREAU` or `SK_ID_PREV`
+
+        grandparent_var : string
+            Variable representing each unique observation in the grandparent.
+            For example, `SK_ID_CURR`.
+
+        df_name : string
+            String for renaming the resulting columns.
+            The columns are name with the `df_name` and with the
+            statistic calculated in the column
+
+    Return
+    --------
+        df_info : dataframe
+            A dataframe with one row for each observation of the grandparent variable.
+            The grandparent variable forms the index, and the resulting dataframe
+            can be merged with the grandparent to be used for training/testing.
+            Columns with all duplicate values are removed from the dataframe before returning.
+
+    """
+
+    # set the parent_var as the index of the parent_df for faster merges
+    parent_df = parent_df[[parent_var, grandparent_var]].copy().set_index(parent_var)
+
+    # Aggregate the numeric variables at the parent level
+    df_agg = agg_numeric(df, parent_var, '%s_LOAN' % df_name)
+
+    # Merge to get the grandparent variable in the data
+    df_agg = df_agg.merge(parent_df,
+                          on=parent_var, how='left')
+
+    # Aggregate the numeric variables at the grandparent level
+    df_agg_client = agg_numeric(df_agg, grandparent_var, '%s_CLIENT' % df_name)
+
+    # Can only apply one-hot encoding to categorical variables
+    if any(df.dtypes == 'category'):
+
+        # Aggregate the categorical variables at the parent level
+        df_agg_cat = agg_categorical(df, parent_var, '%s_LOAN' % df_name)
+        df_agg_cat = df_agg_cat.merge(parent_df,
+                                      on=parent_var, how='left')
+
+        # Aggregate the categorical variables at the grandparent level
+        df_agg_cat_client = agg_numeric(df_agg_cat, grandparent_var, '%s_CLIENT' % df_name)
+        df_info = df_agg_client.merge(df_agg_cat_client, on=grandparent_var, how='outer')
+
+        gc.enable()
+        del df_agg, df_agg_client, df_agg_cat, df_agg_cat_client
+        gc.collect()
+
+    # If there are no categorical variables, then we only need the numeric aggregations
+    else:
+        df_info = df_agg_client.copy()
+
+        gc.enable()
+        del df_agg, df_agg_client
+        gc.collect()
+
+    # Drop the columns with all duplicated values
+    _, idx = np.unique(df_info, axis=1, return_index=True)
+    df_info = df_info.iloc[:, idx]
+
+    return df_info
+
+
+def calculate_correlation(df, col1, col2):
+    return df[col1].corr(df[col2])
+
+
 
 
